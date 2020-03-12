ab570c8974d51c6cc354b27fa0db4db808025aa4
Stepan Novozhilov <hit2hat@icloud.com>
2020-03-12 23:41:55 +0300

    Reformat StatusCard component

--- src/components/StatusCard/index.js
+++ src/components/StatusCard/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import {Card, Cell, Avatar} from '@vkontakte/vkui';
+import PropTypes from 'prop-types';
+import { Card, Cell, Avatar } from '@vkontakte/vkui';
 
-const StatusCard = ({text, installed, owner_name, mine}) => {
-  return (
-      <Card mode='shadow'
-            size='l'
-            style={{padding: 10, marginBottom: 15}}
-      >
-        <Cell description={`Status text: ${text}`}
-              multiline
-              asideContent={installed}
-              before={<Avatar size={52} src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg" /> }>
-          {owner_name}
-        </Cell>
-      </Card>
-  )
+const StatusCard = ({ text, owner, onOpen }) => {
+    return (
+        <Card mode="shadow" size="l" style={{padding: 10, marginBottom: 15}} onClick={onOpen}>
+            <Cell
+                multiline
+                description={text}
+                before={
+                    <Avatar size={52} src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg" />
+                }
+            >
+                {owner.name}
+            </Cell>
+        </Card>
+    )
 };
 
+StatusCard.propTypes = {
+    text: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+    }),
+    onOpen: PropTypes.func,
+};
 
 export default StatusCard;
