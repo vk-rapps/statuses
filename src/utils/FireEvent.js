a02f982b58b5d052a8bc2bdfe3d13cfd8355683c
Stepan Novozhilov <hit2hat@icloud.com>
2020-03-12 22:36:04 +0300

    Add FireEvent util

---
+++ src/utils/FireEvent.js
@@ -0,0 +1,13 @@
+const FireEvent = (link) => {
+    const a = document.createElement('a');
+    a.href = link;
+    a.target = '_blank';
+
+    a.dispatchEvent(new window.MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }));
+};
+
+export default FireEvent;
