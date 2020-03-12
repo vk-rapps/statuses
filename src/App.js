58dcf5a2f3c745b44565c3a0b98126570c3d4014
Stepan Novozhilov <hit2hat@icloud.com>
2020-03-12 23:02:34 +0300

    Add popout into View

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
-import { View } from '@vkontakte/vkui';
+import { View, ScreenSpinner } from '@vkontakte/vkui';
 
 import Home from './containers/Home';
+import Create from './containers/Create';
 
 const App = () => {
-	const [ activePanel, setActivePanel ] = useState("home");
+	const [ activePanel, setActivePanel ] = useState("create");
+	const [ popout, setPopout ] = useState(null);
 
 	const goForward = (nextPanel = null) => {
 		setActivePanel(nextPanel);
 	};
 
+	const showLoader = () => {
+		setPopout(<ScreenSpinner/>);
+		return () => setPopout(null);
+	};
+
 	const navigator = {
 		goForward,
+		showLoader,
 	};
 
 	return (
-		<View activePanel={activePanel}>
+		<View activePanel={activePanel} popout={popout}>
 			<Home id="home" navigator={navigator} />
+			<Create id="create" navigator={navigator} />
 		</View>
 	);
 };
