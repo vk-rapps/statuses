ffb8f83e51c7e0231fc03cac1fd81ac12ca86795
Stepan Novozhilov <hit2hat@icloud.com>
2020-03-13 00:24:03 +0300

    Update root reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import profile from './profile';
 import statuses from './statuses';
 
 export default combineReducers({
+    profile,
     statuses,
 });
